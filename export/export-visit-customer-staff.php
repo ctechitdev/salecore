e6af02b651a2b82203dd2bf12ad965384d705270
ctechitdev <ctech.itdev@gmail.com>
2024-05-07 10:09:26 +0700

    billy-update-export-07052024

--- export/export-visit-customer-staff.php
+++ export/export-visit-customer-staff.php
@@ -32,11 +32,10 @@ $styleArray = array(
 $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'ລຳດັບ');
 $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'ລະຫັດຮ້ານ');
 $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'ຊື່ຮ້ານ');
-$objPHPExcel->getActiveSheet()->SetCellValue('D1', 'ເຈົ້າຂອງຮ້ານ');
-$objPHPExcel->getActiveSheet()->SetCellValue('E1', 'ຊື່ພະນັກງານ');
-$objPHPExcel->getActiveSheet()->SetCellValue('F1', 'ວັນທີ່ຢ້ຽມຢາມ');
-$objPHPExcel->getActiveSheet()->SetCellValue('G1', 'ເວລາເຂົ້າ');
-$objPHPExcel->getActiveSheet()->SetCellValue('H1', 'ເວລາອອກ'); 
+$objPHPExcel->getActiveSheet()->SetCellValue('D1', 'ຊື່ພະນັກງານ');
+$objPHPExcel->getActiveSheet()->SetCellValue('E1', 'ວັນທີ່ຢ້ຽມຢາມ');
+$objPHPExcel->getActiveSheet()->SetCellValue('F1', 'ເວລາເຂົ້າ');
+$objPHPExcel->getActiveSheet()->SetCellValue('G1', 'ເວລາອອກ'); 
 
 $objPHPExcel->getDefaultStyle()->applyFromArray($styleArray);
 
@@ -46,12 +45,13 @@ $rowCount = 2;
 
 
 
-$stmt1 = $conn->prepare(" SELECT cus_code,c_shop_name,c_name,village,distict_name,time_check_in,
+$stmt1 = $conn->prepare(" SELECT b.cus_code,c_shop_name,village_name,distict_name,time_check_in,
 time_check_out,concat(staff_cp,' ',staff_name) as staff_name,date_check
 FROM tbl_visited_customer a
-left join tbl_customer b on a.cus_code = b.c_code
+left join tbl_visit_dairy b on a.cus_code = b.vd_id
 left join tbl_districts c on b.district = c.dis_id
 left join tbl_staff_sale d on a.check_by = d.user_ids
+left join tbl_user_staff e on a.check_by = e.usid
 where  date_check  between '$date_view_from' and '$date_view_to' ");
 $stmt1->execute();
 if ($stmt1->rowCount() > 0) {
@@ -62,11 +62,10 @@ if ($stmt1->rowCount() > 0) {
         $objPHPExcel->getActiveSheet()->SetCellValue('A' . $rowCount, mb_strtoupper($rowCount, 'UTF-8'));
         $objPHPExcel->getActiveSheet()->SetCellValue('B' . $rowCount, mb_strtoupper($row1['cus_code'], 'UTF-8'));
         $objPHPExcel->getActiveSheet()->SetCellValue('C' . $rowCount, mb_strtoupper($row1['c_shop_name'], 'UTF-8'));
-        $objPHPExcel->getActiveSheet()->SetCellValue('D' . $rowCount, mb_strtoupper($row1['c_name'], 'UTF-8'));
-        $objPHPExcel->getActiveSheet()->SetCellValue('E' . $rowCount, mb_strtoupper($row1['staff_name'], 'UTF-8'));
-        $objPHPExcel->getActiveSheet()->SetCellValue('F' . $rowCount, mb_strtoupper($row1['date_check'], 'UTF-8'));
-        $objPHPExcel->getActiveSheet()->SetCellValue('G' . $rowCount, mb_strtoupper($row1['time_check_in'], 'UTF-8')); 
-        $objPHPExcel->getActiveSheet()->SetCellValue('H' . $rowCount, mb_strtoupper($row1['time_check_out'], 'UTF-8')); 
+        $objPHPExcel->getActiveSheet()->SetCellValue('D' . $rowCount, mb_strtoupper($row1['staff_name'], 'UTF-8'));
+        $objPHPExcel->getActiveSheet()->SetCellValue('E' . $rowCount, mb_strtoupper($row1['date_check'], 'UTF-8'));
+        $objPHPExcel->getActiveSheet()->SetCellValue('F' . $rowCount, mb_strtoupper($row1['time_check_in'], 'UTF-8'));
+        $objPHPExcel->getActiveSheet()->SetCellValue('G' . $rowCount, mb_strtoupper($row1['time_check_out'], 'UTF-8'));  
         $rowCount++;
     }
 }
