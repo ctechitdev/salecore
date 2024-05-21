dd7900ecfe9d0e812c2eb7a40a347c0e0734580f
ctechitdev <ctech.itdev@gmail.com>
2024-05-21 12:18:57 +0700

    uploiad-price-21052024

--- page/price-upload.php
+++ page/price-upload.php
@@ -27,17 +27,19 @@ $header_click = "5";
 <script src="../plugins/nprogress/nprogress.js"></script>
 <script type="text/javascript" src="../js/jquery.min.js"></script>
 <script>
-    $(document).on("click", "#editmodal", function(e) {
-        e.preventDefault();
-        var stock_bill_id = $(this).data("stock_bill_id");
+	$(document).on("click", "#editmodal", function(e) {
+		e.preventDefault();
+		var item_code = $(this).data("item_code");
+		var pack_type_name = $(this).data("pack_type_name");
 
-        $.post('../function/modal/show-item-upload-stock.php', {
-                stock_bill_id: stock_bill_id
-            },
-            function(output) {
-                $('.show_data_edit').html(output).show();
-            });
-    });
+		$.post('../function/modal/price-manage.php', {
+				item_code: item_code,
+				pack_type_name: pack_type_name
+			},
+			function(output) {
+				$('.show_data_edit').html(output).show();
+			});
+	});
 </script>
 
 <body class="navbar-fixed sidebar-fixed" id="body">
@@ -65,7 +67,7 @@ $header_click = "5";
                                     <div class="email-body-head text-center text-dark h2 mb-3"><?php echo "$header_name"; ?></div>
                                     <form id="add-bill" method="post" enctype="multipart/form-data">
 
- 
+
 
                                         <div class="input-states">
                                             <div class="col-lg-12">
@@ -102,11 +104,69 @@ $header_click = "5";
 
             <div class="content-wrapper">
                 <div class="content">
-                    <!-- For Components documentaion -->
+
+                    <div class="card card-default">
+
+                        <div class="card-body">
+
+                            <table id="productsTable4" class="table table-hover table-product" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>ເລກລຳດັບ</th>
+                                        <th>ໂຄດສິນຄ້າ</th>
+                                        <th>ຊື່ສິນຄ້າ</th>
+                                        <th>ຫົວໜ່ວຍ</th>
+                                        <th>ລາຄາ</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
 
 
-                     
+                                    $i = 1;
+                                    $stmt1 = $conn->prepare(" select a.item_code,item_name,pack_type_name,sale_price
+									from tbl_item_price_sale a
+									left join tbl_item_code_list b on a.item_code = b.full_code 
+									order by item_name asc ");
+                                    $stmt1->execute();
+                                    if ($stmt1->rowCount() > 0) {
+                                        while ($row1 = $stmt1->fetch(PDO::FETCH_ASSOC)) {
+                                    ?>
 
+                                            <tr>
+
+                                                <td><?php echo  $i; ?></td>
+                                                <td><?php echo  $row1['item_code']; ?></td>
+                                                <td><?php echo  $row1['item_name']; ?></td>
+                                                <td><?php echo  $row1['pack_type_name']; ?></td>
+
+                                                <td><?php echo number_format($row1['sale_price'], 2); ?></td>
+                                                <td>
+                                                    <div class="dropdown">
+                                                        <a class="dropdown-toggle icon-burger-mini" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
+                                                        </a>
+
+                                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuLink">
+                                                            <a href="javascript:0" class="dropdown-item" id="editmodal" data-item_code='<?php echo $row1['item_code']; ?>' data-pack_type_name='<?php echo $row1['pack_type_name']; ?>' data-toggle="modal" data-target="#modal-edit">ປັບລາຄາ</a>
+
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+
+                                    <?php
+                                            $i++;
+                                        }
+                                    }
+                                    ?>
+
+
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
                     <div class="modal fade" id="modal-edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                             <div class="modal-content">
@@ -128,6 +188,7 @@ $header_click = "5";
                             </div>
                         </div>
                     </div>
+
                 </div>
 
             </div>
@@ -252,8 +313,24 @@ $header_click = "5";
 
             return false;
         });
-    </script>
-
+ 
+		$(document).on("submit", "#update-modal", function() {
+			$.post("../query/update-item-price.php", $(this).serialize(), function(data) {
+				if (data.res == "success") {
+					Swal.fire(
+						'ສຳເລັດ',
+						'ປ່ຽນລາຄາສຳເລັດ',
+						'success'
+					)
+					setTimeout(
+						function() {
+							location.reload();
+						}, 1000);
+				}
+			}, 'json')
+			return false;
+		});
+	</script>
 
     <!--  -->
 
